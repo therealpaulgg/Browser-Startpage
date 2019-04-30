b817098a411309be35893cfcdfdc80e509325efe
Paul <paul@paul.systems>
2019-04-30 16:43:27 -0700

    weather icon added

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,65 @@
+/* CODE THAT IS EXECUTED */
+
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?id=5317058&units=metric&APPID=9ec985f43b7fc537d4ab3d4953fb50ed"
 const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?id=5317058&units=metric&APPID=9ec985f43b7fc537d4ab3d4953fb50ed"
 
 let div = document.getElementById("weather")
-let weatherP = document.createElement("h2")
-let dateP = document.createElement("h2")
-let timeP = document.createElement("h2")
+let weatherP = document.getElementById("weatherP")
+weatherP.style = "display: inline;"
+let dateTimeP = document.getElementById("dateTimeP")
+let weatherIcon = document.createElement("i")
 
-main()
+main()	
+let timerId = setInterval(time, 1000)
 
-async function main() {
-	let date = new Date()
+var divider = document.getElementById("links")
+
+links.forEach((item) => {
+	var string = `${item.name} @ ${item.url}`
+	var element = document.createElement("p")
+	element.textContent = string
+	divider.appendChild(element)
+})
+
+/* FUNCTIONS */
+
+function main() {
+	setWeather()
+	setDateTime()	
+}
+
+async function setWeather() {
 	let weatherData = await getWeatherJson()
-	weatherP.textContent = `${weatherData.main.temp} °C in ${weatherData.name} - ${weatherData.weather[0].description}`
-	dateP.textContent = `${date.toLocaleDateString()}`
+	let farenheight = (weatherData.main.temp * (9/5) + 32).toFixed(2)
+	weatherP.textContent = `${farenheight} °F / ${weatherData.main.temp} °C in ${weatherData.name} - ${weatherData.weather[0].description} `
+	console.log(`${weatherData.weather[0].main}, ${weatherData.weather[0].icon}`)
+	let weatherId = weatherData.weather[0].id
+	var icon = weatherIconDict[weatherId].icon	
+	if (!(weatherId >= 700 && weatherId < 800) && !(weatherId >= 900 && weatherId < 1000)) {
+		let date = new Date()
+		if (date.getTime() / 1000 > weatherData.sys.sunset || date.getTime() / 1000 < weatherData.sys.sunrise) icon = "night-" + icon
+		else icon = "day-" + icon
+	}
+	icon = "wi wi-" + icon
+	
+	weatherIcon.className += icon
+	weatherP.appendChild(weatherIcon)
+}
+
+function setDateTime() {
+	let date = new Date()
 	let hrs = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
 	let mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
 	let secs = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
-	timeP.textContent = `${hrs}:${mins}:${secs}`
-	div.appendChild(weatherP)
-	div.appendChild(timeP)
-	div.appendChild(dateP)
-	updateTime()
+	dateTimeP.textContent = `${date.toLocaleDateString()} - ${hrs}:${mins}:${secs}`
 }
-
-let timerId = setInterval(time, 1000)
 
 async function time() {
 	let date = new Date()
 	let hrs = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
 	let mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
 	let secs = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
-	timeP.textContent = `${hrs}:${mins}:${secs}`
+	dateTimeP.textContent = `${date.toLocaleDateString()} - ${hrs}:${mins}:${secs}`
 }
 
 async function getWeatherJson() {
@@ -42,12 +71,3 @@ async function getWeatherJson() {
 				})
 		return json
 }
-
-var divider = document.getElementById("links")
-
-links.forEach((item) => {
-	var string = `${item.name} @ ${item.url}`
-	var element = document.createElement("p")
-	element.textContent = string
-	divider.appendChild(element)
-})
