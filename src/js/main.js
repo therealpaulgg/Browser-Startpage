c5200f875fe476523c1505e677c27eac057000c4
Andrew Paul Gellai Jr <paul@paul.systems>
2019-05-03 01:26:01 -0700

    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
-let weatherUrl// "https://api.openweathermap.org/data/2.5/weather?id=5317058&units=metric&APPID=9ec985f43b7fc537d4ab3d4953fb50ed"
-let forecastUrl// "https://api.openweathermap.org/data/2.5/forecast?id=5317058&units=metric&APPID=9ec985f43b7fc537d4ab3d4953fb50ed"
+// unused variable
+// let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?id=5317058&units=metric&APPID=9ec985f43b7fc537d4ab3d4953fb50ed"
 
 const locationUrl = "https://www.geoip-db.com/json/"
+const MAX_FETCH_RETRIES = 7
 
 // HTML elements that will be modified
 let div = document.getElementById("weather")
@@ -10,53 +11,60 @@ let dateTimeP = document.getElementById("dateTimeP")
 // creation of weather icon
 let weatherIcon = document.createElement("i")
 
-// information on user's location
-let country_code
-let postal
-
 // variables for updating weather status
 let sunrise
 let sunset
 let sunsetSwitch = false
 let sunriseSwitch = false
-let oldUpdateTime
 
-main()	
+main()
 
 /* FUNCTIONS */
 
 async function main() {
-	await getUserLocation()
-	weatherUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${postal},${country_code}&units=metric&APPID=9ec985f43b7fc537d4ab3d4953fb50ed`
+	// look at changes in set weather
+	let ten_minutes_in_millis = 1000 * 10 * 60
+	setIntervalAsync(setWeather, ten_minutes_in_millis)
 	await setWeather()
-	setDateTime()	
-	setInterval(time, 1000)
+	setDateTime()
+	time()
+
 	setLinks()
-	document.getElementById("content").style.display = "contents" 
+	document.getElementById("content").style.display = "contents"
 	document.getElementById("loading").style.display = "none"
 }
 
+function log(val) {
+	console.log(val)
+	return val
+}
+
+// setInterval function that can deal with async
+function setIntervalAsync(fn, interval_in_millis) {
+	fn().then(() => {
+		setTimeout(() => setIntervalAsync(fn, interval_in_millis), interval_in_millis)
+	})
+}
 
 async function getUserLocation() {
 	let json = await getJson(locationUrl)
-	country_code = json.country_code
-	postal = json.postal
+	return [json.country_code, json.postal]
 }
 
 async function setWeather() {
-	let weatherData = await getWeatherJson()
-	let fahrenheit = (weatherData.main.temp * (9/5) + 32).toFixed(2)
+	let [country, postal] = await getUserLocation()
+	let weatherData = await getWeatherJson(country, postal)
+	let fahrenheit = (weatherData.main.temp * (9 / 5) + 32).toFixed(2)
 	weatherP.textContent = `${fahrenheit} °F / ${weatherData.main.temp} °C in ${weatherData.name} - ${weatherData.weather[0].description} `
 	let weatherId = weatherData.weather[0].id
-	let icon = weatherIconDict[weatherId].icon	
+	let icon = weatherIconDict[weatherId].icon
 	if (!(weatherId > 699 && weatherId < 800) && !(weatherId > 899 && weatherId < 1000)) {
 		let date = new Date()
 		if (date.getTime() / 1000 > weatherData.sys.sunset || date.getTime() / 1000 < weatherData.sys.sunrise) {
 			icon = "night-" + icon
-		}
-		else {
+		} else {
 			icon = "day-" + icon
-		}	
+		}
 	}
 
 	icon = "wi wi-" + icon
@@ -67,46 +75,44 @@ async function setWeather() {
 }
 
 function setDateTime() {
-	let date = new Date()
-	oldUpdateTime = date.getTime()
-	let hrs = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
-	let mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
-	let secs = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
-	dateTimeP.textContent = `${date.toLocaleDateString()} - ${hrs}:${mins}:${secs}`
+	let dateTime = new Date()
+	let hrs = dateTime.getHours() < 10 ? `0${dateTime.getHours()}` : dateTime.getHours()
+	let mins = dateTime.getMinutes() < 10 ? `0${dateTime.getMinutes()}` : dateTime.getMinutes()
+	let secs = dateTime.getSeconds() < 10 ? `0${dateTime.getSeconds()}` : dateTime.getSeconds()
+	dateTimeP.textContent = `${dateTime.toLocaleDateString()} - ${hrs}:${mins}:${secs}`
 }
 
-async function time() {
-	let date = new Date()
-	let hrs = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
-	let mins = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
-	let secs = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
-	dateTimeP.textContent = `${date.toLocaleDateString()} - ${hrs}:${mins}:${secs}`
-
-	// if we have waited 10 minutes, update again
-	if (date.getTime() - oldUpdateTime >= 1000 * 60 * 10) {
-		setWeather()
-		oldUpdateTime = date.getTime()
-	}
-
+function time() {
+	let dateTime = new Date()
+	let hrs = timeFormat(dateTime.getHours())
+	let mins = timeFormat(dateTime.getMinutes())
+	let secs = timeFormat(dateTime.getSeconds())
+	dateTimeP.textContent = `${dateTime.toLocaleDateString()} - ${hrs}:${mins}:${secs}`
 	checkWeatherSwitch()
+	setTimeout(time, 1000 - (new Date()).getMilliseconds())
 }
 
-async function checkWeatherSwitch() {
-	midnightCheck()
-	let date = new Date()
-	if (!sunsetSwitch || !sunriseSwitch) {
-		if (date.getTime() / 1000 > sunrise && date.getTime() / 1000 < sunset && !sunriseSwitch) {
-			setWeather()
-			sunriseSwitch = true
-		} else if (date.getTime() / 1000 > sunset && !sunsetSwitch) {
-			setWeather()
-			console.log(`${date.getTime() / 1000}, ${sunset}`)
-			sunsetSwitch = true
+function timeFormat(val) {
+	return val < 10 ? `0${val}` : `${val}`
+}
+
+function checkWeatherSwitch() {
+	if (!midnightCheck()) {
+		let date = new Date()
+		if (!sunsetSwitch || !sunriseSwitch) {
+			if (date.getTime() / 1000 > sunrise && date.getTime() / 1000 < sunset && !sunriseSwitch) {
+				setWeather()
+				sunriseSwitch = true
+			} else if (date.getTime() / 1000 > sunset && !sunsetSwitch) {
+				setWeather()
+				sunsetSwitch = true
+			}
 		}
 	}
 }
 
-async function midnightCheck() {
+// does not call async functions, so removed async keyword
+function midnightCheck() {
 	let newDate = new Date()
 	let newDateStr = newDate.toLocaleDateString()
 	let newDateDay = newDateStr.split("/")[1]
@@ -116,10 +122,11 @@ async function midnightCheck() {
 	oldDate.setUTCMilliseconds(newDate.getTime() - 1000)
 	if (newDateDay != oldDateDay) {
 		setWeather()
-		console.log("uhh")
 		sunriseSwitch = false
 		sunsetSwitch = false
+		return true
 	}
+	return false
 }
 
 function setLinks() {
@@ -128,23 +135,22 @@ function setLinks() {
 	let productivityDiv = document.getElementById("productivity-links")
 	let newsDiv = document.getElementById("news-links")
 
-	let stuff = [
-		{
-			div: socialDiv, 
+	let stuff = [{
+			div: socialDiv,
 			links: socialLinks
-		}, 
+		},
 		{
-			div: mediaDiv, 
+			div: mediaDiv,
 			links: mediaLinks
 		},
 		{
-			div: productivityDiv, 
+			div: productivityDiv,
 			links: productivityLinks
 		},
 		{
-			div: newsDiv, 
+			div: newsDiv,
 			links: newsLinks
-		} 
+		}
 	]
 
 	stuff.forEach((item) => {
@@ -163,17 +169,25 @@ function setLinks() {
 	})
 }
 
-async function getWeatherJson() {
-		let json = await getJson(weatherUrl)
-		return json
+// refactored here
+async function getWeatherJson(country, postal) {
+	let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${postal},${country}&units=metric&APPID=9ec985f43b7fc537d4ab3d4953fb50ed`
+	return await getJson(weatherUrl)
 }
 
 async function getJson(url) {
-	let json = await fetch(url)
-				.then((response) => {
-						if (response.ok) {
-								return response.json()
-						}
-				})
+	let json = null
+	let response = null
+	for (let i = 0; i < MAX_FETCH_RETRIES && json == null; i++) {
+		response = await fetch(url)
+			.catch((error) => {
+				console.log('An error occured while fetching weather data!')
+				console.error(error)
+				return {
+					ok: false
+				}
+			})
+		json = response.ok ? response.json() : null
+	}
 	return json
 }
